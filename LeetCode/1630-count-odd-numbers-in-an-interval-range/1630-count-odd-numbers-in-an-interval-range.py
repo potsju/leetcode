1d859a8beef7745af40e04e2af51db6efc320d8a
darren <70989850+potsju@users.noreply.github.com>
2025-12-06 23:35:50 -0800

    Time: 21 ms (12.77%), Space: 12.4 MB (78.72%) - LeetArchive

---
+++ LeetCode/1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def countOdds(self, low, high):
+        if (high - low + 1) % 2 == 0:
+            return (high - low + 1) // 2
+        else:
+            return (high - low + 1) // 2 + (1 if low % 2 == 1 else 0)
